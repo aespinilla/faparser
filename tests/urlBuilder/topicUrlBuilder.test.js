9ec3826c9ada3bdfe4424609842599ad2ee79390
Alberto Espinilla Garrido <shekigt@gmail.com>
2023-01-24 19:34:29 +0100

    Add topic url builder test

---
+++ tests/urlBuilder/topicUrlBuilder.test.js
@@ -0,0 +1,19 @@
+import { build as topicUrlBuilder } from "../../src/urlBuilder/topicUrlBuilder.js";
+
+describe('Build trailers url', () => {
+    it('should return topic url given lang', () => {
+        const lang = 'xx';
+        const query = 'XXXXXX';
+        const input = {lang, query};
+        const output = topicUrlBuilder(input);
+        expect(output).toBe(`https://www.filmaffinity.com/${lang}/movietopic.php?topic=${query}&attr=rat_count&nodoc`);
+    });
+
+    it('should return topic url', () => {
+        const lang = 'es';
+        const query = 'XXXXXX';
+        const input = { query };
+        const output = topicUrlBuilder(input);
+        expect(output).toBe(`https://www.filmaffinity.com/${lang}/movietopic.php?topic=${query}&attr=rat_count&nodoc`);
+    });
+});
