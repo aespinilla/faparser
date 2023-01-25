5082f605893037fe02f26696ea3510dcbc80b935
Alberto Espinilla Garrido <shekigt@gmail.com>
2023-01-25 19:43:31 +0100

    Add proReviewController test

---
+++ tests/controller/proReviewsController.test.js
@@ -0,0 +1,28 @@
+import { proReviewsController } from "../../src/controller";
+import * as requestMock from '../../src/request/request.js';
+import * as urlBuilderMock from '../../src/urlBuilder/proReviewsUrlBuilder.js';
+import * as parserMock from '../../src/parser/proReviewsParser.js';
+
+describe('Pro reviews controller tests', () => {
+
+    beforeEach(() => {
+        requestMock.request = jest.fn().mockReturnValue(Promise.resolve({
+            url: 'http://fake.address',
+            response: {},
+            body: 'Some body content'
+        }));
+    
+        urlBuilderMock.build = jest.fn().mockReturnValue('http://fake.address');
+    
+        parserMock.parse = jest.fn().mockReturnValue({});
+    });
+
+    it('Should return pro reviews object', async () => {
+        const input = {id: 'XX'};
+        const output = await proReviewsController(input);
+        expect(output).toMatchObject({});
+        expect(requestMock.request).toHaveBeenCalledTimes(1);
+        expect(urlBuilderMock.build).toHaveBeenCalledTimes(1);
+        expect(parserMock.parse).toHaveBeenCalledTimes(1);
+    });
+});
